7f8bfc97f3f1f8f7bf18983cc200af2947e8e1be
BillyJWagner <4684321+inkblot600@users.noreply.github.com>
2018-10-17 21:17:55 -0500

    Initial Build With Swagger Implementation
    Very basic Example with .net 4.71 and Swagger

---
+++ Controllers/SimpleController.cs
@@ -0,0 +1,32 @@
+﻿using APICore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APICore.Controllers
+{
+    public class SimpleController : ApiController
+    {
+        [HttpGet]
+        [Route("api/v1/GetSimpleUser/[name]")]
+        public IHttpActionResult GetUserByName(string name)
+        {
+            return Ok(new Simple
+            {
+                Name = name,
+                Email = name + "@gmail.com"
+            });
+        }
+
+        [HttpPost]
+        [Route("api/v1/AddSimpleuser/")]
+        public IHttpActionResult AddUser(string name)
+        {
+            Simple simple = new Simple();
+            return Ok(name);
+        }
+    }
+}
